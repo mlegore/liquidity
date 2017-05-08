e9d0b3c453842c5e8ac6c640f2d5d3c20a88f309
Michael LeGore <mlegore@outlook.com>
2017-05-08 17:18:41 -0400

    Actual first commit

--- decentralizedOracle.sol
+++ decentralizedOracle.sol
@@ -1,60 +1,59 @@
 pragma solidity ^0.4.0;
 
-/// @title Voting with delegation.
+/// @title True or False Oracle.
 contract BinaryOracle {
 
-    mapping(bool => address) votes; 
+    struct Vote { bool value; bool explicit; }
+    mapping(address => Vote) votes;
     address[] voters;
 
-    bool public value;
+    string public target;
+    bool public value;    
     uint public lastUpdated;
 
-    /// Create a new oracle.
-    function BinaryOracle() {
-        
+    /**@dev Calculates a rectangle's surface and perimeter.
+     * @param _target the target value that voters try to provide.
+     * @return value The value of target as determined by Schelling Point.
+     */
+    function BinaryOracle(string _target) {
+        target = _target;
     }  
 
-    /// Give your vote (including votes delegated to you)
-    /// to proposal `proposals[proposal].name`.
+    /// Give your vote on the possible value of the target.
     function vote(bool value) {
-        votes[msg.sender] = value;
+        votes[msg.sender].value = value;
+        if(!votes[msg.sender].explicit) {
+            uint next = voters.length++;
+            voters[next] = msg.sender;
+            votes[msg.sender].explicit = true;
+        }
 
-        if(voters.length == 101)
-        {            
-            int trueCount = 0;
-            if(votes[voters[i]])
-            {
-                trueCount++;
+        if(voters.length == 101) {            
+            uint trueCount = 0;
+            lastUpdated = now;
+
+            for(uint i = 0; i < voters.length; i++) {
+                if(votes[voters[i]].value) {
+                    trueCount++;
+                }
             }
+           
             value = trueCount > 50;
+            uint reward;
 
-            int reward = this.balance / 101;
-            for(uint i = 0; i < 101; i++)
-            {
-                voters[i].send(reward);
+            // Determine the reward by splitting the balance between the correct voters.
+            if(value) {
+                reward = this.balance / trueCount;
+            } else {
+                reward = this.balance / (101 - trueCount);
+            }
+            
+            for(uint j = 0; j < 101;j++) {
+                // If the voter's response was correct, reward them.
+                if(value == votes[voters[i]].value) {
+                    voters[i].transfer(reward);
+                }
             }            
         }        
-    }
-
-    /// @dev Computes the winning proposal taking all
-    /// previous votes into account.
-    function winningProposal() constant
-            returns (uint winningProposal)
-    {
-        uint winningVoteCount = 0;
-        for (uint p = 0; p < proposals.length; p++) {
-            if (proposals[p].voteCount > winningVoteCount) {
-                winningVoteCount = proposals[p].voteCount;
-                winningProposal = p;
-            }
-        }
-    }
-
-
-
-    function currentValue() constant
-        returns (uint value)
-    {
-        
     }
 }
